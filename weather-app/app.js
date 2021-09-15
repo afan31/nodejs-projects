42b5d205040d6c46022cce2b13e48b7f079e274a
afan31 <afan.khan50@gmail.com>
2021-09-15 16:51:57 -0400

    callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,15 @@
-console.log('Starting');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-console.log('Stopping');
+const address = 'San Francisco, USA'
+geocode(address,(error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+})
+
+const lat =  42.3765;
+const long = 71.2356
+forecast(lat, long, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+})
